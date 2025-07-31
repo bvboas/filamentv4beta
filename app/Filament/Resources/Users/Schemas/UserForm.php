b0d1ed844a0311cc7e77ceaa4785d5095d16b87a
Bento Vilas Boas <bento@licentia.pt>
2025-07-31 16:52:46 +0100

    Initial commit - Filament v4 Beta

---
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Filament\Resources\Users\Schemas;
+
+use Filament\Schemas\Schema;
+
+class UserForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                \Filament\Forms\Components\HIdden::make('id'),
+                \Filament\Forms\Components\TextInput::make('email')
+            ]);
+    }
+}
