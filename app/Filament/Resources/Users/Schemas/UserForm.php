91e95dfd100b6d4a40832910257aa37a6b949970
Bento Vilas Boas <bento@licentia.pt>
2025-08-02 16:44:18 +0100

    Prsisten Tabs not saved

--- app/Filament/Resources/Users/Schemas/UserForm.php
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -2,16 +2,32 @@
 
 namespace App\Filament\Resources\Users\Schemas;
 
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Tabs;
 use Filament\Schemas\Schema;
 
 class UserForm
 {
+
     public static function configure(Schema $schema): Schema
     {
+
         return $schema
             ->components([
-                \Filament\Forms\Components\HIdden::make('id'),
-                \Filament\Forms\Components\TextInput::make('email')
-            ]);
+                    Tabs::make('main')
+                        ->schema(
+                            [
+                                Tabs\Tab::make('name')->schema([
+                                    TextInput::make('name'),
+                                ]),
+                                Tabs\Tab::make('email')->schema([
+                                    TextInput::make('email'),
+                                ]),
+                            ]
+                        )
+                        ->columnSpanFull()
+                        ->persistTabInQueryString(),
+                ]
+            );
     }
 }
